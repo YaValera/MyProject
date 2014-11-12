bdc50aee7474cc57ea9ec6376cd849e287fb14f6
SilinA <zzomband@yandex.ua>
2014-11-12 09:09:25 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace DS_LB1
 {
-    static class Program
+    static class Program//lox
     {
         /// <summary>
         /// The main entry point for the application.
